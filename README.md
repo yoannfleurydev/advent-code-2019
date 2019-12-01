1915466eb60b1e51193ea31db3909d867fcf93d1
Yoann Fleury <yoann.fleury@yahoo.com>
2019-12-01 16:00:50 +0100

    day-01 part 2

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent Of Code 2019
+
+These are my solutions for the Advent Of Code 2019. Each commit is a part of an
+exercise. I'm not sure I'll have time to do every day and every part, but I'll
+try.
+
+If you have any question about my implementation, you can open an issue with
+your question.
--- day-01/index.js
+++ day-01/index.js
@@ -1,5 +1,19 @@
 const fs = require("fs");
 
+function getFuelForMass(mass) {
+  return Math.floor(mass / 3) - 2;
+}
+
+function additionalFuel(fuel) {
+  const addFuel = getFuelForMass(fuel);
+
+  if (addFuel <= 0) {
+    return 0;
+  } else {
+    return addFuel + additionalFuel(addFuel);
+  }
+}
+
 const main = async () => {
   fs.readFile("./data/day-1-input.txt", "utf8", (err, data) => {
     if (err) throw err;
@@ -9,10 +23,10 @@ const main = async () => {
       .map(mass => parseInt(mass))
       .filter(mass => !isNaN(mass));
 
-    const totalFuel = masses.reduce(
-      (fuel, currentMass) => fuel + (Math.floor(currentMass / 3) - 2),
-      0
-    );
+    const totalFuel = masses.reduce((fuel, currentMass) => {
+      const fuelForMass = getFuelForMass(currentMass);
+      return fuel + fuelForMass + additionalFuel(fuelForMass);
+    }, 0);
 
     console.log(`Total fuel needed for modules: ${totalFuel}`);
   });
